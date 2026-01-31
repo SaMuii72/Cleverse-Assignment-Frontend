7e1a4e7fd406ff15d8bfeaee51be29a46c9d6662
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2026-01-31 12:40:49 +0700

    Fix Ui of Logform

--- app/(flightlog)/LogForm.tsx
+++ app/(flightlog)/LogForm.tsx
@@ -44,53 +44,96 @@ function LogForm({ onSubmit }: LogFormProps) {
     }));
   }, []);
 
+  
   return (
-    <div style={{ display: "flex", columnGap: 8 }}>
-      <div
-        style={{ flex: 1, display: "flex", flexDirection: "column", rowGap: 4 }}
-      >
-        <label htmlFor="pname" style={{ fontWeight: "bold" }}>
-          Passenger Name:
+    <div className="flex flex-col gap-4 md:flex-row md:items-end">
+      {/* Passenger */}
+      <div className="flex flex-col gap-1 flex-1">
+        <label htmlFor="passengerName" className="font-bold">
+          Passenger Name
         </label>
         <input
+          id="passengerName"
           type="text"
-          id="pname"
-          name="pname"
+          className="border rounded-md px-3 py-2"
           value={formData.passengerName}
           onChange={handleChange}
         />
       </div>
-      <div
-        style={{ flex: 1, display: "flex", flexDirection: "column", rowGap: 4 }}
-      >
-        <label htmlFor="airport" style={{ fontWeight: "bold" }}>
-          Airport:
+
+      {/* Airport */}
+      <div className="flex flex-col gap-1 flex-1">
+        <label htmlFor="airport" className="font-bold">
+          Airport
         </label>
         <input
-          type="text"
           id="airport"
-          name="airport"
+          type="text"
+          className="border rounded-md px-3 py-2"
           value={formData.airport}
           onChange={handleChange}
         />
       </div>
-      <div
-        style={{ flex: 1, display: "flex", flexDirection: "column", rowGap: 4 }}
-      >
-        <label htmlFor="timestamp" style={{ fontWeight: "bold" }}>
-          Timestamp:
+
+      {/* Timestamp */}
+      <div className="flex flex-col gap-1 flex-1">
+        <label htmlFor="timestamp" className="font-bold">
+          Timestamp
         </label>
         <input
-          type="text"
           id="timestamp"
-          name="timestamp"
+          type="number"
+          className="border rounded-md px-3 py-2"
           value={formData.timestamp}
           onChange={handleChange}
         />
       </div>
-      <div style={{ flex: 1, display: "flex", alignItems: "flex-end" }}>
-        <button onClick={handleSubmit}>Submit</button>
+
+      {/* Type */}
+      <div className="flex flex-col gap-1 flex-1">
+        <label className="font-bold">Type</label>
+        <div className="flex rounded-full border p-1 gap-1.5">
+          <button
+            type="button"
+            onClick={() => setType("departure")}
+            className={`flex-1 py-1 px-3 rounded-full transition
+              ${
+                type === "departure"
+                  ? "bg-[#3EBAD0] text-white"
+                  : "hover:bg-[#3EBAD0]/20"
+              }`}
+          >
+            Departure
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setType("arrival")}
+            className={`flex-1 p-1 rounded-full transition
+              ${
+                type === "arrival"
+                  ? "bg-[#3EBAD0] text-white"
+                  : "hover:bg-[#3EBAD0]/20"
+              }`}
+          >
+            Arrival
+          </button>
+        </div>
       </div>
+
+      {/* Submit */}
+      <button
+        onClick={handleSubmit}
+        disabled={!isFormValid}
+        className={`h-[42px] md:w-auto w-full rounded-lg text-white transition px-3
+          ${
+            isFormValid
+              ? "bg-black hover:bg-[#3EBAD0]"
+              : "bg-gray-400 cursor-not-allowed"
+          }`}
+      >
+        Submit
+      </button>
     </div>
   );
 }
